af309bbd8e7f3b6a746d0bd63270d55189d1ff67
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-12-25 00:34:29 +0530

    created Teacher.java as teacher entity into system

--- src/main/java/model/Teacher.java
+++ src/main/java/model/Teacher.java
@@ -1,5 +1,89 @@
 package model;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="teacher")
 class Teacher{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long teacher_id;
+	
+	private long emp_id;
+	private String name;
+	private String email;
+	private String pass;
+	private String dept;
+	private String status = "ACTIVE";
+	
+	@CreationTimestamp
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime created_at;
+
+	public long getTeacher_id() {
+		return teacher_id;
+	}
+
+	public long getEmp_id() {
+		return emp_id;
+	}
+
+	public void setEmp_id(long emp_id) {
+		this.emp_id = emp_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public String getCreated_at() {
+		return created_at.toString();
+	}
+
 	
 }
