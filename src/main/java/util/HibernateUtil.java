1e4a00ea11939e0d969ff9675a846341c280d354
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-12-25 23:33:39 +0530

    created HibernateUtil to create and manage Hibernate Session

--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -1,6 +1,15 @@
 package util;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-	
+	public static Session getSession() {
+		SessionFactory factory = new Configuration()
+				.configure()
+				.buildSessionFactory();
+		
+		return factory.openSession(); 	
+	}
 }
