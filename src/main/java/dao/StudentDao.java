f59d4f0a0e58034749201d1d34a9152571ecb728
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-12-27 02:00:34 +0530

    Update StudentDao.java

--- src/main/java/dao/StudentDao.java
+++ src/main/java/dao/StudentDao.java
@@ -1,5 +1,20 @@
 package dao;
+import com.sun.tools.classfile.StackMapTable_attribute.verification_type_info;
+
+import model.Student;
 
 public class StudentDao {
-
+	public static void saveStudent(Student student) {}
+	public static void getStudentByEmail(String email) {}
+	public static void getStudentById(int id) {}
+	public static void getAllStudent() {}
+	public static void updateStudent(Student student) {}
+	public static void updateStudentStatus(String status) {}
+	public static void deleteStudent(String status) {}
+	
+	//analytical
+	public static void getTotalStudentCount() {}
+	public static void getStudentsByClass(String course, int semester) {}
+	public static void getRecentStudents(int limit) {}
+	
 }
