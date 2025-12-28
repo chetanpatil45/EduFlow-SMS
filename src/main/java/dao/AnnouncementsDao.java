3e722694b50ab6d7ec4b54c367a60e803cb4b18b
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-12-29 01:07:16 +0530

    update

--- src/main/java/dao/AnnouncementsDao.java
+++ src/main/java/dao/AnnouncementsDao.java
@@ -1,5 +1,23 @@
 package dao;
 
-public class AnnouncementsDao {
+import java.util.List;
+
+import model.Announcement;
+
+public interface AnnouncementsDao {
+	public void save(Announcement announcement);
+	public List<Announcement> findActive();
+	public List<Announcement> findByCourse(Long courseId);
+	
+    public Announcement findById(Long id);
+    public List<Announcement> findAll();
+    public void update(Announcement announcement);
+
+	
+	public List<Announcement> findForAdmin();
+	public List<Announcement> findForTeacher(Long teacherId);
+	public List<Announcement> findForStudent(Long studentId);
+
+	public void deactivate(Long id);
 
 }
