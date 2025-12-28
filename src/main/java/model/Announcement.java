366739c53d75cfa3e0dac2f7cf679c07cd01c317
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-12-28 15:46:10 +0530

    created Announcement model/entity in the system

---
+++ src/main/java/model/Announcement.java
@@ -0,0 +1,98 @@
+package model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "announcements")
+public class Announcement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(length = 1000)
+    private String message;
+
+    private String targetRole; // STUDENT / TEACHER / ALL
+
+    private Long createdBy;
+
+    @CreationTimestamp
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime created_at;
+
+    private LocalDateTime expiresAt;
+
+    private boolean status;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getTargetRole() {
+		return targetRole;
+	}
+
+	public void setTargetRole(String targetRole) {
+		this.targetRole = targetRole;
+	}
+
+	public Long getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(Long createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public LocalDateTime getCreated_at() {
+		return created_at;
+	}
+
+
+
+	public LocalDateTime getExpiresAt() {
+		return expiresAt;
+	}
+
+	public void setExpiresAt(LocalDateTime expiresAt) {
+		this.expiresAt = expiresAt;
+//		this.expiresAt = LocalDateTime.now(); --> current date time
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+}
+
