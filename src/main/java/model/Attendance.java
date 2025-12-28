22d668d249a6db5506d4e2c315520a3c7741e49b
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-12-29 00:57:34 +0530

    Update Attendance.java

--- src/main/java/model/Attendance.java
+++ src/main/java/model/Attendance.java
@@ -1,5 +1,82 @@
 package model;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "attendance")
 public class Attendance {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long studentId;
+
+    private Long teacherId;
+
+    private Long courseId;
+
+    private LocalDate date;
+
+    private String status; // PRESENT / ABSENT
+
+    private LocalDateTime markedAt;
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public Long getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Long studentId) {
+		this.studentId = studentId;
+	}
+
+	public Long getTeacherId() {
+		return teacherId;
+	}
+
+	public void setTeacherId(Long teacherId) {
+		this.teacherId = teacherId;
+	}
+
+	public Long getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(Long courseId) {
+		this.courseId = courseId;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getMarkedAt() {
+		return markedAt;
+	}
+
+
+    
+    
 }
+
