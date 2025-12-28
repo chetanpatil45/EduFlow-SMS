acb735ce4e112abb688659b65ba53ff67f207cb2
Chetanpatil03 <chetanpatil6303@gmail.com>
2025-12-29 00:57:41 +0530

    Update AttendanceDao.java

--- src/main/java/dao/AttendanceDao.java
+++ src/main/java/dao/AttendanceDao.java
@@ -1,5 +1,16 @@
 package dao;
 
-public class AttendanceDao {
+import java.util.List;
+
+import model.Student;
+
+public interface AttendanceDao {
+	public long getTotalClasses(Long studentId, Long courseId);
+
+	public long getPresentCount(Long studentId, Long courseId);
+
+	public double getAttendancePercentage(Long studentId, Long courseId);
+
+	public List<Student> getDefaulters(Long courseId, double minPercentage);
 
 }
